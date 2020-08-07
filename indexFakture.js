b9f28cf951f320ccd4a6fe20d434d10ee3ac4fb2
Marija Bozic <marijabozic@outlook.com>
2020-08-07 22:14:45 +0200

    izmenjen jos deo

--- indexFakture.js
+++ indexFakture.js
@@ -1,10 +1,30 @@
 const fakture = []
 let count = 0
 
+
 // Inputi (posto sve to vec u HTMlu stoji na pravi se, nego samo selektuje)
+
+const unetiIme = document.querySelector('#txtCmpnName')
+const unetiPIB = document.querySelector('#txtCmpnPIB')
+const unosDatuma = document.querySelector('#txtTime')
+const unetaVrednot = document.querySelector('#txtValue')
+const dgmDodaj = document.querySelector('#bttnAdd')
 
 
 // Dugme (dodatni poeni..)
 
+const sveFakture = document.querySelector('#sveFakt')
+
 
 // Lista za ubacivanje faktura
+
+const lista = document.querySelector('#itemList')
+
+
+// Neajbitniji deo koda (?)
+
+
+
+
+// // Dodatno 
+// sveFakture.
